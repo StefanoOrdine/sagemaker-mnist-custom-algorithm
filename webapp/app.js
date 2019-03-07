b2849667b9a2d3e23e0c1250467753b5deb7bdd3
Walter Dal Mut <walter.dalmut@gmail.com>
2019-03-07 21:53:14 +0100

    Added upload pictures

--- webapp/app.js
+++ webapp/app.js
@@ -34,6 +34,9 @@ function sendData() {
 
   data = R.map(R.compose(R.ifElse(R.lt(0), R.flip(R.divide)(3), R.identity), R.sum, R.slice(0,3)), R.splitEvery(4, data))
 
+  console.log(data)
+
+
   data = new Uint8Array(data);
 
   $.ajax({
@@ -86,5 +89,29 @@ function redraw(){
     context.closePath();
     context.stroke();
   }
+}
+
+let imageLoader = document.getElementById('imageLoader');
+    imageLoader.addEventListener('change', handleImage, false);
+
+
+function handleImage(e){
+  let canvas = document.getElementById('canvas');
+  let ctx = canvas.getContext('2d');
+  ctx.fillStyle = "#FFF";
+
+  var reader = new FileReader();
+  reader.onload = function(event){
+    let img = R.splitEvery(28, atob(event.srcElement.result.split(",").slice(-1).pop().trim()))
+
+    for (let i=0; i<28; i++) {
+      for (let j=0; j<28; j++) {
+        if (img[i][j].charCodeAt(0)) {
+          ctx.fillRect(j,i,1,1);
+        }
+      }
+    }
+  }
+  reader.readAsDataURL(e.target.files[0]);
 }
 
